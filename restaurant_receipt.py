043539839a5ca6e641e638fb912d17e1e73ce162
samuelkwabenafrimpong8 <samuelkwabenafrimpong8@gmail.com>
2026-07-23 23:52:39 +0000

    Add files via upload

---
+++ restaurant_receipt.py
@@ -0,0 +1,13 @@
+name = "Kay and Fedra Restaurant."
+customer_name = "Kwabena Frimpong"
+
+print(name)
+print(customer_name)
+
+print("Grilled Tilapia/Banku   350.00")
+print("Chicken Wings           150.00")
+print("Malta Guinness          50.00")
+total = 350+150+50
+print("Total:", total)
+
+print("THANK YOU")
