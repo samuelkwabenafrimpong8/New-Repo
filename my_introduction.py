083998de2b5e4a6f57af281b15cbc95841aec7b9
samuelkwabenafrimpong8 <samuelkwabenafrimpong8@gmail.com>
2026-07-23 23:39:11 +0000

    my work

---
+++ my_introduction.py
@@ -0,0 +1,11 @@
+# Is to collect information from user or a database
+# To run logic, do math, sort information, or make decisions
+# To display the result to the user, save a file, or send the data or printing the text
+
+print("Samuek Kwabena Frimpong")
+print("Mathimatical Sceince")
+print("Level 100")
+print("Ashongman Estate")
+print("Banku with Okra stew")
+print("I'm here to build my fundation in python. My goal is to start with the basics like scripting and simple programs, and eventually use it for web development and data.")
+print("My dream is to become a software developer")
